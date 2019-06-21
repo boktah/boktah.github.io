f38faa7529c29d13300730566611c9d8a3c8cc39
Taha Bokhari <tbokhar@ncsu.edu>
2019-06-21 10:21:10 -0400

    Corrected path to css files

--- scripts/colortheme.js
+++ scripts/colortheme.js
@@ -6,8 +6,8 @@ function initate() {
     var style1 = document.getElementById("dark");
     var style2 = document.getElementById("light");
 
-    style1.onclick = function () { swapStyleSheet("default.css"); };
-    style2.onclick = function () { swapStyleSheet("light.css"); };
+    style1.onclick = function () { swapStyleSheet("/assets/css/default.css"); };
+    style2.onclick = function () { swapStyleSheet("/assets/css/light.css"); };
 }
 
 window.onload = initate;
