17bc2218649637d2bea5940781750d8227a2733f
Taha Bokhari <tbokhar@ncsu.edu>
2019-06-21 10:06:28 -0400

    Update colortheme.js

--- scripts/colortheme.js
+++ scripts/colortheme.js
@@ -3,8 +3,8 @@ function swapStyleSheet(sheet) {
 }
 
 function initate() {
-    var style1 = document.getElementById("stylesheet1");
-    var style2 = document.getElementById("stylesheet2");
+    var style1 = document.getElementById("dark");
+    var style2 = document.getElementById("light");
 
     style1.onclick = swapStyleSheet("default.css");
     style2.onclick = swapStyleSheet("light.css");
