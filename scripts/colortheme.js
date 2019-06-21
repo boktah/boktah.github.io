3faac78caa55f1fa347663fe1cc9c1baacef61d5
Taha Bokhari <tbokhar@ncsu.edu>
2019-06-21 10:41:59 -0400

    Potential theme save script

--- scripts/colortheme.js
+++ scripts/colortheme.js
@@ -1,14 +1,28 @@
+var dark = true;
+
 function swapStyleSheet(sheet) {
     document.getElementById("pagestyle").setAttribute("href", sheet);  
 }
 
-function initate() {
+function initiate() {
     var style1 = document.getElementById("dark");
     var style2 = document.getElementById("light");
 
-    style1.onclick = function () { swapStyleSheet("/assets/css/default.css"); };
-    style2.onclick = function () { swapStyleSheet("/assets/css/light.css"); };
+    style1.onclick = function () {
+        swapStyleSheet("/assets/css/default.css");
+        dark = true;
+    };
+    style2.onclick = function () {
+        swapStyleSheet("/assets/css/light.css");
+        dark = false;
+    };
 }
 
-// window.onload = initate;
-window.onload = function () { console.log("load"); };
+window.onload = function () {
+    if (dark) {
+        swapStyleSheet("/assets/css/dark.css");
+    } else {
+        swapStyleSheet("/assets/css/light.css");
+    }
+    window.onload = initiate;
+};
